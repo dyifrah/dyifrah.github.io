74fe76c72e6962a98bcd13af7765171826da7096
dyifrah <dyifrah1@gmail.com>
2017-04-05 17:27:08 -0400

    Commit with Scroll

--- js/main.js
+++ js/main.js
@@ -1,10 +1,66 @@
 $(function() {
 
-  $('#dialog').hide();
-  $('#section3').click(dialogBox);
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 
-  function dialogBox {
-    $('#dialog').dialog();
-  }
+    $('#collapse a').click(hideNavPanel);
 
+    function hideNavPanel() {
+        $('.navbar-toggle').addClass('collapsed');
+        $('#collapse').removeClass('in');
+    }
+
+
+    // opened 
+    // navbar-toggle
+    // navbar-collapse collapse in
+
+    // closed
+    // navbar-toggle collapsed
+    // navbar-collapse collapse
+
+
+    // $('.responsive').slick({
+    //   dots: true,
+    //   infinite: false,
+    //   speed: 300,
+    //   slidesToShow: 4,
+    //   slidesToScroll: 4,
+    //   responsive: [
+    //     {
+    //       breakpoint: 1024,
+    //       settings: {
+    //         slidesToShow: 3,
+    //         slidesToScroll: 3,
+    //         infinite: true,
+    //         dots: true
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 600,
+    //       settings: {
+    //         slidesToShow: 2,
+    //         slidesToScroll: 2
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 480,
+    //       settings: {
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1
+    //       }
+    //     }
+
+    //   ]
+    // });
 });
